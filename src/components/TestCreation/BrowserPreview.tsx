1a5bd8cf7f9efde70a847d6a2ab44df24be9266c
Patricio Albornoz <pwalbornoz@gmail.com>
2025-03-02 17:43:09 -0300

    edit waiting

--- src/components/TestCreation/BrowserPreview.tsx
+++ src/components/TestCreation/BrowserPreview.tsx
@@ -38,12 +38,12 @@ const BrowserPreview: React.FC<BrowserPreviewProps> = ({
               className="object-contain w-full h-full"
             />
           ) : (
-            <div className="text-gray-400">
+            <div className="text-gray-400 flex flex-col gap-4">
               {isRunning ? "Loading..." : "Run tests to see browser preview"}
             </div>
           )}
           {isRunning && status !== "complete" && (
-            <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-20">
+            <div className="absolute inset-0 flex items-center justify-center bg-cover bg-center bg-no-repeat bg-[url('https://gifdb.com/images/high/dancing-white-duck-hip-hop-dance-491s86yb1ef6ra20.webp')]">
               <div className="w-8 h-8 border-4 border-t-cyan-500 border-r-transparent border-b-transparent border-l-transparent rounded-full animate-spin"></div>
             </div>
           )}
